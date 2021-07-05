43af88ceb5f31a5b4265ecbc8bd86753f30c1a25
hatskier <suvorovalex1997@gmail.com>
2021-07-05 14:31:22 +0200

    token lists updated

--- tools/generateTokenList.js
+++ tools/generateTokenList.js
@@ -11,12 +11,12 @@ async function main(version = 2) {
       console.log(`Processing: ${symbol}.`);
       const addresses = getAddresses(symbol);
       if (addresses) {
-      console.log('Address: ' + addresses.redstoneProxy);
+      console.log("Address: " + addresses.redstoneProxy);
       template.tokens.push({
         "chainId": 42,
         "address": addresses.redstoneProxy,
         "symbol": symbol,
-        "name": commodities[symbol].name,
+        "name": getCommodityName(symbol),
         "decimals": 18,
         "logoURI": commodities[symbol].logoURI,
         "tags": [
@@ -35,7 +35,21 @@ async function main(version = 2) {
 
   const filename = "tokenListGenerated.json";
   console.log(`=== Saving report to a file: ${filename} ===`);
-  fs.writeFileSync(filename, JSON.stringify(template, null, 2), { flag: 'w' });
+  fs.writeFileSync(filename, JSON.stringify(template, null, 2), { flag: "w" });
+}
+
+function getCommodityName(symbol) {
+  const charsToRemove = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "#", "(", ")"];
+  const originalName = commodities[symbol].name;
+  return removeChars(originalName, charsToRemove);
+}
+
+function removeChars(str, charsToRemove) {
+  let result = str;
+  for (const charToRemove of charsToRemove) {
+    result = result.replace(charToRemove, "");
+  }
+  return result;
 }
 
 function getAddresses(symbol) {
